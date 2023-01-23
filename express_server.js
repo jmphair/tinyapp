008a185ea124540f5bb760f67c8bb23387162985
Jonathan Phair <jmphair@gmail.com>
2023-01-23 15:52:37 -0500

    added use and post functionality

--- express_server.js
+++ express_server.js
@@ -9,6 +9,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// needs to come BEFORE all of our routes bc we need the middleware to parse the data before it's needed by the routes!
+
+app.use(express.urlencoded({ extended: true }));
+
+// good rule of thumb to follow is that route should be ordered from most to least specific
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -26,12 +32,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+  
+}
