03d5bebdeb1108d2950ca708b348c9d148fa1c70
Jonathan Phair <jmphair@gmail.com>
2023-01-24 18:57:43 -0500

    login header updated

--- express_server.js
+++ express_server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -17,6 +20,7 @@ const urlDatabase = {
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,16 +35,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
