418a3b7962fb0940179fa4cf810a837ef66f7f7e
Jonathan Phair <jmphair@gmail.com>
2023-01-25 18:19:58 -0500

    fixed the header logout button, initially removed the form elements as instructed but didn't realize I would need a form to have the button with its methods etc stay intact

--- express_server.js
+++ express_server.js
@@ -30,12 +30,12 @@ const users = {
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur",
+    password: "123",
   },
   user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk",
+    password: "456",
   },
 };
 
