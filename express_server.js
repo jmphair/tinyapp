bd5ffc4cff9007e143a7849c9bec52966c01e2c6
Jonathan Phair <jmphair@gmail.com>
2023-01-25 12:51:08 -0500

    added morgan dev dependency to help with logs

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const morgan = require('morgan');
+
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -34,6 +36,8 @@ const users = {
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
